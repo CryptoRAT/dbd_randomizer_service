4cd5ae6d37efba6d7b856fbd7f057b07314373b6
Luke Elliot <lmelliot@gmail.com>
2023-11-25 11:52:34 -0500

    updated dependencies to include all previous. There should be a keyword for this.

--- perk/migrations/0004_alter_perk_type.py
+++ perk/migrations/0004_alter_perk_type.py
@@ -6,6 +6,8 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('perk', '0001_initial'),
+        ('perk', '0002_add_perk_data'),
         ('perk', '0003_add_general_perk_data'),
     ]
 
