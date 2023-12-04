732ad6cc25a4548e15b7ef27647dc9af9971b728
Luke Elliot <lmelliot@gmail.com>
2023-12-04 17:49:47 -0500

    increasing debug option in prod

--- dbd_randomizer_service/settings_prod.py
+++ dbd_randomizer_service/settings_prod.py
@@ -1,3 +1,4 @@
 CSRF_TRUSTED_ORIGINS = ['https://cryptorat.com', 'https://www.cryptorat.com', 'clownfish-app-8qi77.ondigitalocean.app']
 CSRF_ALLOWED_ORIGINS = ['https://cryptorat.com', 'https://www.cryptorat.com','clownfish-app-8qi77.ondigitalocean.app']
 CORS_ORIGINS_WHITELIST = ['https://cryptorat.com', 'https://www.cryptorat.com','clownfish-app-8qi77.ondigitalocean.app']
+DEBUG = True
