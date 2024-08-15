88da2af7a2618a5b8987ed5e862687bc7fe38d31
Luke Elliot <luke@blendedsense.com>
2024-08-15 13:07:34 -0400

    added some echo statements for sanity check when starting.

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+ENVIRONMENT = os.environ.get('DJANGO_ENV', 'development')
+HOSTS_ALLOWED = os.environ.get('ALLOWED_HOSTS')
+echo "Environment..."
+echo $ENVIRONMENT
+echo "Allowed hosts..."
+echo $HOSTS_ALLOWED
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
